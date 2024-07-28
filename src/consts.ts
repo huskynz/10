098ef762fd6d75c3d9bb4a6268a914e3d2a9a3f4
Peter Gallwas <peter@husky.nz>
2024-07-28 22:33:41 +1200

    Add content

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "HuskyNZ | Peter",
+  DESCRIPTION: "Who am I?.",
+  AUTHOR: "Peter Gallwas",
 }
 
 // Work Page
@@ -37,11 +37,7 @@ export const LINKS: Links = [
     TEXT: "Home", 
     HREF: "/", 
   },
-  { 
-    TEXT: "Work", 
-    HREF: "/work", 
-  },
-  { 
+  {
     TEXT: "Blog", 
     HREF: "/blog", 
   },
@@ -56,26 +52,20 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "peter@husky.nz",
+    HREF: "mailto:peter@husky.nz",
   },
-  { 
-    NAME: "Github",
+  {
+    NAME: "Main Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "HuskyNZ",
+    HREF: "https://github.com/HuskyNZ"
   },
-  { 
-    NAME: "LinkedIn",
-    ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
-  },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
+  {
+    NAME: "Github Personal",
+    ICON: "github",
+    TEXT: "Husky-Devel",
+    HREF: "https://github.com/husky-devel"
+  }
 ]
 
