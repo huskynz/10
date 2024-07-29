28fb71d19cb76cee52310fd93ad330f89ab132f2
Peter Gallwas <peter@husky.nz>
2024-07-29 19:27:17 +1200

    fix it now?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,9 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.husky.nz",
-  integrations: [react(),mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
