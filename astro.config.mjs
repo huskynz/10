12a1dc66ef7a94c42d1860b634cb032ac06c9c6f
Peter Gallwas <peter@husky.nz>
2024-07-30 22:52:19 +1200

    change qoute over to new thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,13 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import react from '@astrojs/react';
 
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.husky.nz",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({
+  integrations: [react(),mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
   })],
-  output: "server",
-  adapter: cloudflare()
 });
