5251272a423e55099ca7610f9398bc00295c9323
Peter Gallwas <peter@husky.nz>
2024-08-11 22:21:51 +1200

    fix types

--- src/lib/rurl.ts
+++ src/lib/rurl.ts
@@ -1,4 +1,8 @@
-export const predefinedUrls = {
+export type PredefinedUrls = {
+    [key: string]: string;
+  };
+
+export const predefinedUrls: PredefinedUrls = {
     logo: 'https://serv.husky.nz/logo/default.png',
     github: 'https://github.com/huskynz/',
     panel: 'https://panel.hnz.li'
