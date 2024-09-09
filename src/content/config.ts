0900b3ff58b5eb23b07af678404773357b83dee9
Peter Gallwas <peter@husky.nz>
2024-09-10 01:01:47 +1200

    Start work on first blog item

--- src/content/config.ts
+++ src/content/config.ts
@@ -24,13 +24,10 @@ const blog = defineCollection({
 const projects = defineCollection({
   type: "content",
   schema: z.object({
-    title: z.string(),
-    summary: z.string(),
-    date: z.coerce.date(),
-    tags: z.array(z.string()),
-    draft: z.boolean().optional(),
-    demoUrl: z.string().optional(),
-    repoUrl: z.string().optional(),
+    company: z.string(),
+    role: z.string(),
+    dateStart: z.coerce.date(),
+    dateEnd: z.union([z.coerce.date(), z.string()]),
   }),
 })
 
