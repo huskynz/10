eeeb7715f7cecf52f9fda068e4eab2bec649f530
Peter Gallwas <peter@husky.nz>
2024-10-29 10:56:49 +1300

    Ensure we are importing node in our config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import netlify from "@astrojs/netlify";
+import node from '@astrojs/node';
 
 
 // https://astro.build/config
